2ef1d8412514e86045ad50a3369cb81f146d20bf
Karen Cofre <karencofrecejas@gmail.com>
2023-11-05 22:11:43 -0300

    se hizo vista postulacion

--- resources/views/postulacion.blade.php
+++ resources/views/postulacion.blade.php
@@ -1,5 +1,64 @@
 @extends('app');
 
 @section('content')
-<h2>CV's</h2>
+
+<div class="container">
+    <h1 class="my-4">Build Your CV</h1>
+    <form>
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" id="name" placeholder="Enter your name">
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" placeholder="Enter your email">
+        </div>
+        <div class="mb-3">
+            <label for="phone" class="form-label">Phone Number</label>
+            <input type="text" class="form-control" id="phone" placeholder="Enter your phone number">
+        </div>
+        <div class="mb-3">
+            <label for="acercaDe">Cuéntanos acerca de ti:</label>
+            <textarea class="form-control" id="acercaDe" name="acercaDe" rows="5"></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="cv" class="form-label">Upload Your CV</label>
+            <input class="form-control" type="file" id="cv">
+        </div>
+        <div class="mb-3">
+            <h2>Experiencia Laboral:</h2>
+            <label for="cargo" class="form-label">Cargo</label>
+            <input type="text" class="form-control" id="cargo" name="cargo">
+            <label for="fecha_inicio_e" class="form-label">Fecha de inicio</label>
+            <input type="text" class="form-control" id="fecha_inicio_e" name="fecha_inicio_e">
+            <label for="fecha_fin_e" class="form-label">Fecha de fin</label>
+            <input type="text" class="form-control" id="fecha_fin_e" name="fecha_fin_e">
+            <label for="descripcion">Descripcion:</label>
+            <textarea class="form-control" id="descripcion" name="descripcion" rows="2"></textarea>
+        </div>
+        <div class="mb-3">
+            <h2>Formacion Academica:</h2>
+            <label for="institucion" class="form-label">Institucion</label>
+            <input type="text" class="form-control" id="institucion" name="institucion">
+            <label for="fecha_inicio_f" class="form-label">Fecha de inicio</label>
+            <input type="text" class="form-control" id="fecha_inicio_f" name="fecha_inicio_f">
+            <label for="fecha_fin_f" class="form-label">Fecha de fin</label>
+            <input type="text" class="form-control" id="fecha_fin_f" name="fecha_fin_f">
+            <label for="descripcion">Descripcion:</label>
+            <textarea class="form-control" id="descripcion" name="descripcion" rows="2"></textarea>
+        </div>
+        <div class="mb-3">
+            <h2>Expectativa de renta:</h2>
+            <label for="expectativa" class="form-label">Expectativa</label>
+            <input type="text" class="form-control" id="expectativa" name="expectativa">
+        </div>
+        <div class="container">
+            <div class="row">
+                <button type="submit" class="btn btn-primary col-md-12 my-2">Guardar</button>
+                <button type="submit" class="btn btn-primary col-md-12">Actualizar</button>
+            </div>
+        </div>
+
+    </form>
+</div>
 @endsection
