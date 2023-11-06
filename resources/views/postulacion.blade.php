390406f90e0fda46c96b255898a5f16727170aff
Karen Cofre <karencofrecejas@gmail.com>
2023-11-05 22:24:04 -0300

    agregue 2 form en postulacion

--- resources/views/postulacion.blade.php
+++ resources/views/postulacion.blade.php
@@ -25,17 +25,23 @@
             <label for="cv" class="form-label">Upload Your CV</label>
             <input class="form-control" type="file" id="cv">
         </div>
+
+
         <div class="mb-3">
-            <h2>Experiencia Laboral:</h2>
-            <label for="cargo" class="form-label">Cargo</label>
-            <input type="text" class="form-control" id="cargo" name="cargo">
-            <label for="fecha_inicio_e" class="form-label">Fecha de inicio</label>
-            <input type="text" class="form-control" id="fecha_inicio_e" name="fecha_inicio_e">
-            <label for="fecha_fin_e" class="form-label">Fecha de fin</label>
-            <input type="text" class="form-control" id="fecha_fin_e" name="fecha_fin_e">
-            <label for="descripcion">Descripcion:</label>
-            <textarea class="form-control" id="descripcion" name="descripcion" rows="2"></textarea>
+            <h2>Expectativa de renta:</h2>
+            <label for="expectativa" class="form-label">Expectativa</label>
+            <input type="text" class="form-control" id="expectativa" name="expectativa">
         </div>
+        <div class="container">
+            <div class="row">
+                <button type="submit" class="btn btn-primary col-md-12 my-2">Guardar</button>
+                <button type="submit" class="btn btn-primary col-md-12">Actualizar</button>
+            </div>
+        </div>
+
+    </form>
+
+    <form action="">
         <div class="mb-3">
             <h2>Formacion Academica:</h2>
             <label for="institucion" class="form-label">Institucion</label>
@@ -47,10 +53,26 @@
             <label for="descripcion">Descripcion:</label>
             <textarea class="form-control" id="descripcion" name="descripcion" rows="2"></textarea>
         </div>
+        <div class="container">
+            <div class="row">
+                <button type="submit" class="btn btn-primary col-md-12 my-2">Guardar</button>
+                <button type="submit" class="btn btn-primary col-md-12">Actualizar</button>
+            </div>
+        </div>
+
+    </form>
+
+    <form action="">
         <div class="mb-3">
-            <h2>Expectativa de renta:</h2>
-            <label for="expectativa" class="form-label">Expectativa</label>
-            <input type="text" class="form-control" id="expectativa" name="expectativa">
+            <h2>Experiencia Laboral:</h2>
+            <label for="cargo" class="form-label">Cargo</label>
+            <input type="text" class="form-control" id="cargo" name="cargo">
+            <label for="fecha_inicio_e" class="form-label">Fecha de inicio</label>
+            <input type="text" class="form-control" id="fecha_inicio_e" name="fecha_inicio_e">
+            <label for="fecha_fin_e" class="form-label">Fecha de fin</label>
+            <input type="text" class="form-control" id="fecha_fin_e" name="fecha_fin_e">
+            <label for="descripcion">Descripcion:</label>
+            <textarea class="form-control" id="descripcion" name="descripcion" rows="2"></textarea>
         </div>
         <div class="container">
             <div class="row">
