4a6342e4a82b952f75294361a78e374748e2e826
Karen Cofre <karencofrecejas@gmail.com>
2023-11-05 21:52:22 -0300

    se hizo vista curriculums

--- resources/views/curriculums.blade.php
+++ resources/views/curriculums.blade.php
@@ -1,5 +1,24 @@
 @extends('app');
 
 @section('content')
-<h2>Curr</h2>
+<h2>Postulantes</h2>
+<div class="container mt-5 mb-5">
+    <div class="row">
+        <@php
+
+            $trabajadores = App\Trabajador::all();
+            foreach ($trabajadores as $objeto) {
+            echo '<div class="col-md-3 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">' . $objeto["nombre"] . '</h5>
+                            <p class="card-text">Renta: $' . $objeto["renta"] . '</p>
+                        </div>
+                    </div>
+                </div>';
+        }
+        ?>
+        @endphp
+    </div>
+</div>
 @endsection
